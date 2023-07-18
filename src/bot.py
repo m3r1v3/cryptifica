3d5232b80a88ac78ac8a98e928d40215699a8a03
Maxim Meriberg <merive.official@gmail.com>
2023-07-18 16:01:48 +0300

    Remove title method in show_price func in bot.py
    :sparkles: feat: Remove title method in show_price func in bot.py

--- src/bot.py
+++ src/bot.py
@@ -98,7 +98,7 @@ async def price_option_next(query):
 
 async def show_price(query):
     price = get_price(query.data[6:])
-    name = get_name(query.data[6:]).title()
+    name = get_name(query.data[6:])
     symbol = get_symbol(query.data[6:]).upper()
     percent = '{0:.{1}f}'.format(float(get_change_percent(query.data[6:])), 4)
 
