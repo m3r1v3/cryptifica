60c35a36f2ed5134bb6a1d7d4d1cc729c4ef8e80
Max Meriberg <merive.official@gmail.com>
2023-07-25 12:04:34 +0300

    Update chart font in chart.py
    :sparkles: feat: Update chart font in chart.py
    :sparkles: feat: Update colors in chart.py

--- src/chart.py
+++ src/chart.py
@@ -7,24 +7,24 @@ import pandas as pd
 def get_chart(data, price) -> str:
     df = pd.DataFrame({"date": data, "price": price})
 
-    fig = px.area(df, x='date', y="price", color_discrete_sequence=[f"{'#a5787a' if price[0] > price[-1] else '#7ea578'}"], range_y=[min(price), max(price)])
+    fig = px.area(df, x='date', y="price", color_discrete_sequence=[f"{'#a67b77' if price[0] > price[-1] else '#87a677'}"], range_y=[min(price), max(price)])
     fig.add_hline(y=price[-1],
                   line_dash="dot",
-                  line_color="#D0D0D9",
+                  line_color="#D2D3D9",
                   annotation_text=f"${'{:,}'.format(price[-1])}",
-                  annotation_position="bottom right",
-                  annotation_font_color="#D0D0D9")
+                  annotation_position="top right",
+                  annotation_font_color="#D2D3D9")
     
-    fig.update_xaxes(showgrid=False, visible=True, showticklabels=True, gridcolor="#D0D0D9")
-    fig.update_yaxes(showgrid=True, visible=True, showticklabels=True, gridcolor="#D0D0D9")
-    fig.update_layout(font_family="verdana",
+    fig.update_xaxes(showgrid=False, visible=True, showticklabels=True, gridcolor="#D2D3D9")
+    fig.update_yaxes(showgrid=True, visible=True, showticklabels=True, gridcolor="#D2D3D9")
+    fig.update_layout(font_family="Ruberoid Medium",
                       yaxis_title=None,
                       xaxis_title=None,
                       plot_bgcolor='#181526',
                       paper_bgcolor="#181526",
                       margin=dict(l=40, r=40, t=25, b=25),
-                      yaxis = dict(color = "#D0D0D9"),
-                      xaxis = dict(color = "#D0D0D9"),
+                      yaxis = dict(color = "#D2D3D9"),
+                      xaxis = dict(color = "#D2D3D9"),
                       xaxis_tickformat = '%b %d',
                       yaxis_tickprefix = '$')
     
