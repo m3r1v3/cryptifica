de10b0eb673918342035059f3630141b008f69b7
Max Meriberg <merive.official@gmail.com>
2023-07-27 16:52:02 +0300

    Set default value for coin_id in get_data function in crypto.py
    :sparkles: feat: Set default value for coin_id in get_data function in crypto.py

--- src/crypto.py
+++ src/crypto.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 
 
-def get_data(coin_id: str):
+def get_data(coin_id: str = ""):
     return requests.get(f"https://api.coincap.io/v2/assets/{coin_id}").json()["data"]
 
 
