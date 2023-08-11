fc02e576057420b7137e8e116327969691f994f2
Max Meriberg <merive.official@gmail.com>
2023-08-11 12:48:20 +0300

    Refactor code in chart.py
    :recycle: refactor: Refactor code in chart.py

--- src/chart.py
+++ src/chart.py
@@ -5,32 +5,38 @@ import pandas as pd
 
 
 def get_chart(data, price) -> str:
-    df = pd.DataFrame({"date": data, "price": price})
+    file_name = str(uuid.uuid4())
 
-    fig = px.area(df, x='date', y="price",
-                  color_discrete_sequence=[f"{'#a67b77' if price[0] > price[-1] else '#87a677'}"],
-                  range_y=[min(price), max(price)])
-    fig.add_hline(y=price[-1],
-                  line_dash="dot",
-                  line_color="#D2D3D9",
-                  annotation_text=f"${'{:,}'.format(price[-1])}",
-                  annotation_position="top right",
-                  annotation_font_color="#D2D3D9")
+    fig = make_chart(pd.DataFrame({'date': data, 'price': price}))
+    fig.write_image(f'images/{file_name}.webp', width=1280, height=720)
+
+    return file_name
+
+
+def make_chart(df: pd.DataFrame):
+    first, last = df['price'][0], df['price'][df['price'].count()-1]
+    min, max = df['price'].min(), df['price'].max()
+
+    fig = px.area(df, x='date', y='price',
+                  color_discrete_sequence=[f"{'#a67b77' if first > last else '#87a677'}"],
+                  range_y=[min, max])
+    fig.add_hline(y=last,
+                  line_dash='dot',
+                  line_color='#D2D3D9',
+                  annotation_text=f"${'{:,}'.format(last)}",
+                  annotation_position='top right',
+                  annotation_font_color='#D2D3D9')
 
     fig.update_xaxes(showgrid=False, visible=True, showticklabels=True, gridcolor="#D2D3D9")
-    fig.update_yaxes(showgrid=True, visible=True, showticklabels=True, gridcolor="#D2D3D9")
-    fig.update_layout(font_family="Ruberoid Medium",
+    fig.update_yaxes(showgrid=True, visible=True, showticklabels=True, gridcolor='#D2D3D9')
+    fig.update_layout(font_family='Ruberoid Medium',
                       yaxis_title=None,
                       xaxis_title=None,
                       plot_bgcolor='#181526',
-                      paper_bgcolor="#181526",
+                      paper_bgcolor='#181526',
                       margin=dict(l=40, r=40, t=25, b=25),
-                      yaxis=dict(color="#D2D3D9"),
-                      xaxis=dict(color="#D2D3D9"),
+                      yaxis=dict(color='#D2D3D9'),
+                      xaxis=dict(color='#D2D3D9'),
                       xaxis_tickformat='%b %d',
                       yaxis_tickprefix='$')
-
-    file_name = str(uuid.uuid4())
-    fig.write_image(f"images/{file_name}.webp", width=1280, height=720)
-
-    return file_name
+    return fig
